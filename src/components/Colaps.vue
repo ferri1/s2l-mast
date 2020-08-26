62d401e58b81a4ab44c800cff6ec9d7c8ede03e7
Ferri19 <lyndaferri07@gmail.com>
2020-08-26 14:27:47 +0200

    correctrion de colaps

--- src/components/Colaps.vue
+++ src/components/Colaps.vue
@@ -1,29 +1,40 @@
 <template >
   <div  role = "tablist" >
+      <div >
+       <b-button  v-b-toggle.collapse-1  class = "m-1" >Qu’est-ce que Ange de gain en France ? </b-button >
+       <b-collapse  id = "collapse-1" >
+         <b-card > Je devrais commencez ouvert! </b-card >
+       </b-collapse >
+     </div >
+
+     <div >
+        <b-button  v-b-toggle.collapse-2  class = "m-1" >Comment rejoindre l'Ange de gain ? </b-button >
+        <b-collapse    id = "collapse-2" >
+          <b-card > Je devrais commencez ouvert! </b-card >
+        </b-collapse >
+    </div >
+
+
+     <div >
+        <b-button  v-b-toggle.collapse-3  class = "m-1" >Est ce qu'on garantit le succès des projets présentés ?</b-button >
+        <b-collapse  id = "collapse-3" >
+          <b-card > Je devrais commencez ouvert! </b-card >
+        </b-collapse >
+    </div >
+
     <div >
-   <b-button  v-b-toggle.collapse-3  class = "m-1" > Pour quoi nous?  </b-button >
-   <b-collapse  visible  id = "collapse-3" >
-     <b-card > Je devrais commencez ouvert! </b-card >
-   </b-collapse >
- </div >
- <div >
-<b-button  v-b-toggle.collapse-3  class = "m-1" > quels sont les avantages? </b-button >
-<b-collapse  visible  id = "collapse-3" >
-  <b-card > Je devrais commencez ouvert! </b-card >
-</b-collapse >
-</div >
-<div >
-<b-button  v-b-toggle.collapse-3  class = "m-1" > Toggle Collapse </b-button >
-<b-collapse  visible  id = "collapse-3" >
- <b-card > Je devrais commencez ouvert! </b-card >
-</b-collapse >
-</div >
-<div >
-<b-button  v-b-toggle.collapse-3  class = "m-1" > Toggle Collapse </b-button >
-<b-collapse  visible  id = "collapse-3" >
- <b-card > Je devrais commencez ouvert! </b-card >
-</b-collapse >
-</div >
+        <b-button  v-b-toggle.collapse-4  class = "m-1" >c'est quoi La rénumeration des apporteurs d'affaire? </b-button >
+        <b-collapse   id = "collapse-4" >
+         <b-card > Je devrais commencez ouvert! </b-card >
+        </b-collapse >
+    </div >
+
+    <div >
+        <b-button  v-b-toggle.collapse-5  class = "m-1" >Quelle est la règlementation applicable ? </b-button >
+        <b-collapse    id = "collapse-5" >
+         <b-card > Je devrais commencez ouvert! </b-card >
+        </b-collapse >
+    </div >
   </div >
 </template >
 
@@ -35,3 +46,9 @@ export default {
 
 };
 </script >
+
+<style media="screen">
+.m-1{
+  width: 50em!important;
+}
+</style>
